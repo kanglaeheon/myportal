246086b82efa22667ba43b82e02f9a75081859d5
kanglaeheon <kanglaeheon@gmail.com>
2021-09-28 15:51:41 +0900

    Board Service 수정

--- myportal/src/main/java/com/bitacademy/myportal/service/BoardService.java
+++ myportal/src/main/java/com/bitacademy/myportal/service/BoardService.java
@@ -4,10 +4,10 @@ import java.util.List;
 
 import com.bitacademy.myportal.repository.BoardVo;
 
+//	TODO: Delete 기능 구현해야하는지 파악 후 결정
 public interface BoardService {
 	public List<BoardVo> getList();	//	게시판 목록 보기
 	public BoardVo getContent(Long no);	//	게시물 조회
 	public boolean write(BoardVo vo);	//	게시물 작성 기능
 	public boolean update(BoardVo vo);	//	제시물 수정
-	public boolean delete(BoardVo vo);	//	게시물 삭제
 }
