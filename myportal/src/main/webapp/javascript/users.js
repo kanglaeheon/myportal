186691acfd96d3fa8780327ed46e349500cf077d
kanglaeheon <kanglaeheon@gmail.com>
2021-10-01 11:41:09 +0900

    이메일 중복 체크를 위한 자바스크립트 추가

---
+++ myportal/src/main/webapp/javascript/users.js
@@ -0,0 +1,11 @@
+function checkEmail(obj, url) {
+	//	obj: object (this는 button을 의미)
+	//	url: ajax api 호출 url
+	if (obj.form.email.value.trim().length === 0) {
+		alert("이메일을 입력하세요");
+		return;
+	}
+	
+	//	TODO: Ajax로 url에 중복 이메일 검사
+	
+}
